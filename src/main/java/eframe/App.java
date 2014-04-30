e66cf45addbe789a9ec60150fd9f5025c0854b59
iranran <liweimilan@163.com>
2014-04-30 19:47:51 +0800

    first commit

---
+++ src/main/java/eframe/App.java
@@ -0,0 +1,13 @@
+package eframe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/eframe/jedisq/Jedisq.java
@@ -0,0 +1,13 @@
+package eframe.jedisq;
+
+public class Jedisq {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
